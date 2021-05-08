7b2ce83d9340a47ebdd80e25007303f30261dabb
Dan Carpenter <drdcarpenter@googlemail.com>
2021-05-08 21:03:23 +0100

    first commit with example map

---
+++ webmap.R
@@ -0,0 +1,15 @@
+library(leaflet)
+library(leaflet.esri)
+library(htmlwidgets)
+
+m <- leaflet() %>% 
+  addTiles(group = "Basemap") %>% 
+  setView(-1.2576985, 51.751975, 12) %>% 
+  addEsriFeatureLayer(url = "https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/SSSI_England/FeatureServer/0",
+                      popupProperty = "SSSI_NAME",
+                      group = "Natural England") %>% 
+  addLayersControl(baseGroups = "Basemap", overlayGroups = "Natural England")
+
+m
+
+saveWidget(m, file = "map.html")
