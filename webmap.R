d857a89a9ded8ff3c0a781b1df33a466a04a700f
Dan Carpenter <drdcarpenter@googlemail.com>
2021-05-09 20:32:11 +0100

    add shapefile data to map

--- webmap.R
+++ webmap.R
@@ -1,14 +1,24 @@
 library(leaflet)
 library(leaflet.esri)
 library(htmlwidgets)
+library(sf)
+library(rgdal)
+
+
+# load vector data --------------------------------------------------------
+
+sac <- st_read("data/SACs.shp")
+
+# create webmap -----------------------------------------------------------
 
 m <- leaflet() %>% 
   addTiles(group = "Basemap") %>% 
-  setView(-1.2576985, 51.751975, 12) %>% 
+  setView(-1.2576985, 51.751975, 11) %>% 
   addEsriFeatureLayer(url = "https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/SSSI_England/FeatureServer/0",
                       popupProperty = "SSSI_NAME",
-                      group = "Natural England") %>% 
-  addLayersControl(baseGroups = "Basemap", overlayGroups = "Natural England")
+                      group = "SSSI") %>% 
+  addPolygons(data = sac, group = "SAC", color = "red") %>% 
+  addLayersControl(baseGroups = "Basemap", overlayGroups = c("SSSI", "SAC"))
 
 m
 
